bb77ad899ff9510a30fe0982d4b439ee2439e0eb
linkoten <catto.francois@hotmail.fr>
2024-07-28 08:16:03 +0200

    Projet terminé, problème de key dans le dashboard ?

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,8 @@
 "use client"
 
+export const experimental_ppr = true;
+
+
 import SideNav from '@/app/ui/dashboard/sidenav';
  
 export default function Layout({ children }: { children: React.ReactNode }) {
